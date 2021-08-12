3fafd5835fd882d5357b4a8799d8162a5047394f
labelname <enquiries_garcia@pm.me>
2021-08-12 23:29:20 +0200

    Add project files.

---
+++ QuantConnect_Algorithm/QuantConnect_Algorithm.py
@@ -0,0 +1,16 @@
+class MarkowitzMinimumVolatilityInstabilityAnalyses(QCAlgorithm):
+
+    def Initialize(self):
+        self.SetStartDate(2021, 2, 11)  # Set Start Date
+        self.SetCash(100000)  # Set Strategy Cash
+        # self.AddEquity("SPY", Resolution.Minute)
+
+
+    def OnData(self, data):
+        '''OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
+            Arguments:
+                data: Slice object keyed by symbol containing the stock data
+        '''
+
+        # if not self.Portfolio.Invested:
+        #    self.SetHoldings("SPY", 1)
